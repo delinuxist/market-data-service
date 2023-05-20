b813f351af3f174d87eb589563679b589fbbd234
amnuhu <abdul-majid.nuhu@turntabl.io>
2023-05-20 16:40:04 +0000

    order book service

---
+++ src/main/java/com/tradingengine/marketdataservice/services/OrderBookService.java
@@ -0,0 +1,20 @@
+package com.tradingengine.marketdataservice.services;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+
+@Service
+public class OrderBookService {
+
+
+
+
+    public void getOpenTradesFromExchange() {
+
+
+    }
+
+}
